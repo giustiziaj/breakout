8855c9f7119fc4f8cc58f11bde922f9fcc605fa8
Joe Giustizia <jgiustiz@uwo.ca>
2018-11-28 19:58:32 -0500

    added doxygen for baseobject

--- include/BaseObject.hpp
+++ include/BaseObject.hpp
@@ -3,16 +3,47 @@
 #include <string>
 
 #include "Game.hpp"
-class Game;
+class Game; // Forward declaration for cyclic class
 
+/**
+ * @brief Parent class for all game objects
+ *
+ */
 class BaseObject {
 public:
+  /**
+   * @brief method called on every game object, every frame
+   *
+   */
   virtual void step() {};
+  /**
+   * @brief Get the Sprite object
+   *
+   * @return sf::Sprite*
+   */
   sf::Sprite* getSprite();
+  /**
+   * @brief check for collision between objects
+   *
+   * @return true if there is a collision
+   * @return false otherwise
+   */
   bool collides(BaseObject*);
+  /**
+   * @brief Get the Name object
+   *
+   * @return std::string
+   */
   std::string getName() const;
 
 protected:
+  /**
+   * @brief Construct a new Base Object object
+   *
+   * @param object name
+   * @param starting position
+   * @param texture path
+   */
   BaseObject(std::string, sf::Vector2f, std::string);
   sf::Sprite* sprite;
   std::string name;
